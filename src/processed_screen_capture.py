93870e0818839d50bb3c4fb935f31033f4ad25de
amiteshdeshpande <amiteshdeshpande94@gmail.com>
2019-03-26 14:08:25 -0400

    Added frozen inference graph obtained from training edge_detection images

--- src/processed_screen_capture.py
+++ src/processed_screen_capture.py
@@ -9,10 +9,12 @@ def process_img(image):
     original_image = image
     # convert to opencv
     processed_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    processed_img = cv2.resize(processed_img, (0, 0), fx=0.5, fy=0.5)
     # convert to gray
     processed_img = cv2.cvtColor(processed_img, cv2.COLOR_BGR2GRAY)
     # edge detection
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+
     return processed_img
 
 
@@ -22,7 +24,7 @@ def main():
         screen = pyautogui.screenshot()
         new_screen = process_img(screen)
         cv2.imshow('img',new_screen)
-        cv2.imwrite('./edge_detection_images/img' + str(num) + '.png', new_screen)
+        cv2.imwrite('./test_images/img' + str(num) + '.png', new_screen)
         num += 1
 
 
